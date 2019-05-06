826a6c8c93195ff1a2effa8925cf6b2779226126
Laura Daugherty <lauradaugherty6@gmail.com>
2019-05-06 14:48:26 -0700

    function conversions done

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -68,7 +68,7 @@ console.log('Car 33 is a ' + inventory[32].car_year + inventory[32].car_make + i
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = inventory[inventory.length - 1];
-console.log(lastCar.car_make + lastCar.car_model);
+//console.log(lastCar.car_make + lastCar.car_model);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
@@ -77,7 +77,7 @@ for (i=0; i<inventory.length; i++){
     carModels.push(inventory[i].car_model);
     }
 carModels = carModels.sort();
-console.log(carModels);
+//console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
@@ -86,7 +86,7 @@ for (let i=0; i<inventory.length; i++){
     carYears.push(inventory[i].car_year);
     }
 
-console.log(carYears);
+//console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -96,7 +96,7 @@ for (let i=0; i<carYears.length; i++) {
         oldCars.push(carYears[i]);
     }
 }
-console.log(oldCars); 
+//console.log(oldCars); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
@@ -107,7 +107,7 @@ for (let i=0; i<inventory.length; i++) {
         BMWAndAudi.push(inventory[i]);
     }
 }
-console.log(BMWAndAudi);
+//console.log(BMWAndAudi);
 
 
 
